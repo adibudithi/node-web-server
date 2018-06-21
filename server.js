08bc90948624f0db2c4aec167bedda0866db12f5
adibudithi <adibudithi@gmail.com>
2018-06-21 15:28:59 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${request.method} ${request.url}`;
+  console.log(log);
+
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if (error) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  next();
+});
+
+app.use((request, response, next) => {
+  response.render('maintainance.hbs', {
+    maintainanceMessage: "We'll be right back!",
+    updateMessage: "The site is currently being updated."
+  });
+});
+
+app.use(express.static(__dirname + '/public;'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website!'
+  });
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    error: 'unable to fulfill request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
